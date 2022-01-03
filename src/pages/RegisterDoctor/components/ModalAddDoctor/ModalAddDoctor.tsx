0bf1c47fce4f2419e41157acd4169864ab10162f
Manoel <manoel.farias@aluno.ufop.edu.br>
2022-01-03 17:09:53 -0300

    wip cad doctor

--- src/pages/RegisterDoctor/components/ModalAddDoctor/ModalAddDoctor.tsx
+++ src/pages/RegisterDoctor/components/ModalAddDoctor/ModalAddDoctor.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from 'react';
+import React, { useCallback } from 'react';
 import {
   Button,
   Checkbox,
@@ -15,137 +15,153 @@ import {
   TextField,
 } from '@material-ui/core';
 import { toast } from 'react-toastify';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+import ReactInputMask from 'react-input-mask';
 
 import * as S from './styles';
-import { ImodalAddDoctor } from './types';
+import { IAddDoctor, ImodalAddDoctor } from './types';
 import { addDoc, collection } from '@firebase/firestore';
 import { db } from 'services';
 
 const ModalAddDoctor = (props: ImodalAddDoctor) => {
   const { open, onClose } = props;
   const doctorsCollectionRef = collection(db, 'doctor');
-
-  const [NovoNome, setNovoNome] = useState<string>('');
-  const [NovoEspecialidade, setNovoEspecialidade] = useState<string>('');
-  const [NovoCpf, setNovoCpf] = useState<string>('');
-  const [NovoCrm, setNovoCrm] = useState<string>('');
-  const [NovoCelular, setNovoCelular] = useState<string>('');
-  const [NovoAtendimento, setNovoAtendimento] = useState<string>('');
+  const timeCollectionRef = collection(db, 'time');
 
   const handleClose = useCallback(() => {
     onClose();
   }, [onClose]);
 
-  const creatDoctor = async () => {
-    try {
-      await addDoc(doctorsCollectionRef, {
-        nome: NovoNome,
-        especialidade: NovoEspecialidade,
-        cpf: NovoCpf,
-        crm: NovoCrm,
-        celular: NovoCelular,
-        atendimento: NovoAtendimento,
-      });
-      toast.success('Cadastro realizado!');
-      onClose();
-    } catch (error: any) {
-      toast.error(`${error?.message?.split(':').slice(-1)[0].trim() ?? 'Erro ao criar registro'}`);
-      console.log({ error });
-    }
-  };
+  const formik = useFormik<IAddDoctor>({
+    initialValues: {
+      nome: '',
+      especialidade: '',
+      crm: '',
+      celular: '',
+      atendimento: '',
+      cpf: '',
+    },
+    validateOnBlur: false,
+    validateOnChange: false,
+    enableReinitialize: true,
+    validationSchema: Yup.object().shape({
+      nome: Yup.string().required('Campo Obrigatório'),
+      especialidade: Yup.string().required('Campo Obrigatório'),
+      crm: Yup.string().required('Campo Obrigatório'),
+      celular: Yup.string(),
+      atendimento: Yup.string().required('Campo Obrigatório'),
+      cpf: Yup.string().required('Campo Obrigatório'),
+    }),
+    onSubmit: async (values) => {
+      try {
+        await addDoc(doctorsCollectionRef, values);
+        toast.success('Cadastro realizado!');
+        onClose();
+      } catch (error: any) {
+        toast.error(`${error?.message?.split(':').slice(-1)[0].trim() ?? 'Erro ao criar registro'}`);
+        console.log({ error });
+      }
+    },
+  });
+
+  const { getFieldProps } = formik;
 
   return (
     <S.Container>
       <Dialog open={open} onClose={onClose}>
-        <DialogTitle>Cadastrar Medico</DialogTitle>
-        <Divider />
-        <DialogContent>
-          <Grid container spacing={2}>
-            <Grid item container spacing={1}>
-              <Grid item container xs={12} md={6}>
-                <TextField
-                  label="Nome do Medico"
-                  variant="outlined"
-                  size="small"
-                  fullWidth
-                  onChange={(event) => {
-                    setNovoNome(event.target.value);
-                  }}
-                ></TextField>
-              </Grid>
-              <Grid item xs={12} md={6}>
-                <TextField
-                  label="Especialidade"
-                  variant="outlined"
-                  size="small"
-                  fullWidth
-                  onChange={(event) => {
-                    setNovoEspecialidade(event.target.value);
-                  }}
-                >
-                  {' '}
-                </TextField>
-              </Grid>
-              <Grid item container xs={12} spacing={1}>
-                <Grid item xs={12} md={8}>
+        <form onSubmit={formik.handleSubmit}>
+          <DialogTitle>Cadastrar Medico</DialogTitle>
+          <Divider />
+          <DialogContent>
+            <Grid container spacing={2}>
+              <Grid item container spacing={1}>
+                <Grid item container xs={12} md={12}>
                   <TextField
-                    label="CPF"
+                    label="Nome do Medico"
                     variant="outlined"
                     size="small"
                     fullWidth
-                    onChange={(event) => {
-                      setNovoCpf(event.target.value);
-                    }}
+                    {...getFieldProps('nome')}
+                    error={!!formik.errors.nome}
+                    helperText={formik.errors.nome}
                   ></TextField>
                 </Grid>
-                <Grid item xs={12} md={4}>
+                <Grid item xs={12} md={6}>
+                  <TextField
+                    label="Especialidade"
+                    variant="outlined"
+                    size="small"
+                    fullWidth
+                    {...getFieldProps('especialidade')}
+                    error={!!formik.errors.especialidade}
+                    helperText={formik.errors.especialidade}
+                  />
+                </Grid>
+                <Grid item xs={12} md={6}>
+                  <ReactInputMask mask="999.999.999-99" {...getFieldProps('cpf')}>
+                    {(inputProps: any) => (
+                      <TextField
+                        label="CPF"
+                        variant="outlined"
+                        size="small"
+                        fullWidth
+                        {...inputProps}
+                        error={!!formik.errors.cpf}
+                        helperText={formik.errors.cpf}
+                      />
+                    )}
+                  </ReactInputMask>
+                </Grid>
+
+                <Grid item xs={12} md={6}>
+                  <ReactInputMask mask="(99)99999-9999" {...getFieldProps('celular')}>
+                    {(inputProps: any) => (
+                      <TextField label="celular" variant="outlined" size="small" fullWidth {...inputProps} />
+                    )}
+                  </ReactInputMask>
+                </Grid>
+                <Grid item xs={12} md={6}>
                   <TextField
                     label="CRM"
                     variant="outlined"
                     size="small"
                     fullWidth
-                    onChange={(event) => {
-                      setNovoCrm(event.target.value);
-                    }}
+                    {...getFieldProps('crm')}
+                    error={!!formik.errors.crm}
+                    helperText={formik.errors.crm}
                   ></TextField>
                 </Grid>
-              </Grid>
-              <Grid item container xs={12} spacing={1}>
-                <Grid item xs={12} md={5}>
-                  <TextField
-                    label="Celular"
-                    variant="outlined"
-                    size="small"
-                    fullWidth
-                    onChange={(event) => {
-                      setNovoCelular(event.target.value);
-                    }}
-                  />
-                </Grid>
-                <Grid item xs={12} md={7}>
+                <Grid item xs={12} md={12}>
                   <FormControl component="fieldset">
-                    <FormLabel component="legend">horarios de atendimento</FormLabel>
+                    <FormLabel component="legend">horarios de atendimento: </FormLabel>
                     <FormGroup row>
-                      <FormControlLabel value="top" control={<Checkbox />} label="13h" labelPlacement="top" />
-                      <FormControlLabel value="top" control={<Checkbox />} label="14h" labelPlacement="top" />
-                      <FormControlLabel value="top" control={<Checkbox />} label="15h" labelPlacement="top" />
-                      <FormControlLabel value="top" control={<Checkbox />} label="16h" labelPlacement="top" />
-                      <FormControlLabel value="top" control={<Checkbox />} label="17h" labelPlacement="top" />
+                      <FormControlLabel
+                        {...getFieldProps('atendimento')}
+                        control={<Checkbox />}
+                        value={13}
+                        label="13h"
+                        labelPlacement="bottom"
+                      />
+                      <FormControlLabel value="14h" control={<Checkbox />} label="14h" labelPlacement="bottom" />
+                      <FormControlLabel value="15h" control={<Checkbox />} label="15h" labelPlacement="bottom" />
+                      <FormControlLabel value="16h" control={<Checkbox />} label="16h" labelPlacement="bottom" />
+                      <FormControlLabel value="17h" control={<Checkbox />} label="17h" labelPlacement="bottom" />
                     </FormGroup>
                   </FormControl>
                 </Grid>
               </Grid>
             </Grid>
-          </Grid>
-        </DialogContent>
-        <DialogActions>
-          <Button variant="outlined" color="error" onClick={handleClose}>
-            Cancelar
-          </Button>
-          <Button variant="contained" onClick={creatDoctor} color="success">
-            Salvar
-          </Button>
-        </DialogActions>
+          </DialogContent>
+          <DialogActions>
+            <Button variant="outlined" color="error" onClick={handleClose}>
+              Cancelar
+            </Button>
+            <Button variant="contained" type="submit" color="success">
+              Salvar
+            </Button>
+          </DialogActions>
+        </form>
       </Dialog>
     </S.Container>
   );
--- src/pages/RegisterDoctor/components/ModalAddDoctor/types.ts
+++ src/pages/RegisterDoctor/components/ModalAddDoctor/types.ts
@@ -2,3 +2,12 @@ export interface ImodalAddDoctor {
   open: boolean;
   onClose: () => void;
 }
+
+export interface IAddDoctor {
+  nome: string;
+  especialidade: string;
+  crm: string;
+  celular: string;
+  atendimento: string;
+  cpf: string;
+}
