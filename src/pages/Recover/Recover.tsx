9319a13f2890190e294f8778ce945434dda3c68a
Manoel <manoel.farias@aluno.ufop.edu.br>
2022-01-05 16:56:06 -0300

    recover

--- src/pages/Recover/Recover.tsx
+++ src/pages/Recover/Recover.tsx
@@ -14,7 +14,7 @@ const Recover = () => {
   const forgotPassword = async () => {
     try {
       await sendPasswordResetEmail(auth, email, {
-        url: `http://localhost:3000/signin`,
+        url: `https://tcc-app-9g295uhel-manoeljunim.vercel.app/signin`,
       });
       toast.success(`As instruções para alterar a senha foram enviadas para ${email}`);
       history.push('/signin');
